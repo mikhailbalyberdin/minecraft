9d4bcc7de8330d2767876b07d489f7265fba4653
Misha <mikhailbalyberdin@gmail.com>
2024-09-01 08:55:51 -0400

    feat: add audio for screamer and adapt cards

--- js/screamer.js
+++ js/screamer.js
@@ -3,7 +3,26 @@ const buttonScreamer = document.querySelector("#buttonStartScreamer");
 function showScreamer() {
     const screamerPicture = document.querySelector("#screamer");
     screamerPicture.classList.add("open");
+
+    const audioPlayer = new Audio();
+    audioPlayer.src = "../audio/screamer_sound.mp3";
+    audioPlayer.volume = 0.2;
+    audioPlayer.play();
 }
 
 buttonScreamer.addEventListener("click", showScreamer);
+
+// const userInfo = {
+//     name: "Misha",
+//     age: 15,
+//     address: {
+//         country: "USA",
+//         city: "Cary",
+//     },
+//     sayHi: function() {
+//         console.log(`My name is ${this.name}`);
+//     }
+// }
+
+// userInfo.sayHi();
 
